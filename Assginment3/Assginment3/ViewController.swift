2c4dae3cdb1cc8e4e346d99fab902bbcd0f01565
Kazuya <shiozaki5621@gmail.com>
2019-04-23 01:02:23 -0700

    add assginment3

---
+++ Assginment3/Assginment3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assginment3
+//
+//  Created by SubaruShiozaki on 2019-04-22.
+//  Copyright © 2019 Kazuya Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
